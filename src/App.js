280b252baf1e9a5792f63c956a55adf9f19a4436
amjacc <akhil.m.jayarajan@accenture.com>
2025-03-06 15:47:43 +0530

    login ui

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route index element={<Login />} />
           <Route path="/home" element={<SideNavigation />} />
